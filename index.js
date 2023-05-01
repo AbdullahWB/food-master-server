00cbdbd992d3ffe3c1ffeef0ba2b89731f7d6d6a
AbdullahWB <abdullah917828@gmail.com>
2023-05-02 00:01:48 +0600

    chefs data loade

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 3000;
 
-
+const chefs = require('./data/chef.json')
 
 app.use(cors());
 
 app.get('/', (req, res) => { 
     res.send('food pagla')
+})
+
+app.get('/chefs', (req, res) => { 
+    res.send(chefs)
 })
 
 
