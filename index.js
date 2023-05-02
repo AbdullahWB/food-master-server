7f9f79d54b7a4ec4b20df0ce7f10d3c125aef4b5
AbdullahWB <abdullah917828@gmail.com>
2023-05-02 19:00:33 +0600

    add food json

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const port = process.env.PORT || 3000;
 
 const chefs = require('./data/chef.json')
+const teams = require('./data/team.json')
+const foods = require('./data/food.json')
 
 app.use(cors());
 
@@ -15,6 +17,13 @@ app.get('/chefs', (req, res) => {
     res.send(chefs)
 })
 
+app.get('/teams', (req, res) => { 
+    res.send(teams)
+})
+
+app.get('/foods', (req, res) => { 
+    res.send(foods)
+})
 
 app.listen(port, () => {
     console.log(`food pagla is running on port ${port}`);
