8cb38e622d9d49833dbf824326dbf346df30fcf5
AbdullahWB <abdullah917828@gmail.com>
2023-05-03 14:31:15 +0600

    add some new locatin

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 const chefs = require('./data/chef.json')
 const teams = require('./data/team.json')
 const foods = require('./data/food.json')
+const chefFood = require('./data/chefFood.json')
 
 app.use(cors());
 
@@ -17,12 +18,30 @@ app.get('/chefs', (req, res) => {
     res.send(chefs)
 })
 
+app.get('/chefs/:id', (req, res) => { 
+    const id = req.params.id;
+    const selectedId = chefs.find(c => c.id == id);
+    res.send(selectedId)
+})
+
 app.get('/teams', (req, res) => { 
     res.send(teams)
 })
 
 app.get('/foods', (req, res) => { 
     res.send(foods)
+})
+
+app.get('/categories/category/:id', (req, res) => { 
+    const id = req.params.id;
+    const selectedFood = chefFood.find(f => f.id == id);
+    res.send(selectedFood)
+})
+
+app.get('/categories/:id', (req, res) => { 
+    const id = req.params.id;
+    const selectedType = chefFood.filter(h => h.category_id == id)
+    res.send(selectedType)
 })
 
 app.listen(port, () => {
